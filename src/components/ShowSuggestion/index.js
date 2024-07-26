1ee614f8dc6f446c1b12f1f4bf1221ddd8eb5324
AnushaRupender <splash.anu@gmail.com>
2024-07-26 14:29:54 +0530

    css changes

--- src/components/ShowSuggestion/index.js
+++ src/components/ShowSuggestion/index.js
@@ -3,24 +3,27 @@ import React from "react";
 const ShowSuggestion = (props) => {
     const {suggestionsData} = props;
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>Object</th>
-                    <th>Carbon Footprint</th>
-                    <th>View Suggestions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Object.keys(suggestionsData).map(key=>(
-                    <tr key={key}>
-                        <td>{suggestionsData[key].label}</td>
-                        <td dangerouslySetInnerHTML={{ __html: suggestionsData[key]['Carbon Footprints']}}></td>
-                        <td dangerouslySetInnerHTML={{ __html: suggestionsData[key]['Alternative Idea']}}></td>
-                    </tr>))
-                }
-            </tbody>
-        </table>
+        <>
+            <h3><u>CO<sub>2</sub>e Analysis</u></h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Object</th>
+                        <th>Carbon Footprint</th>
+                        <th>View Suggestions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(suggestionsData).map(key=>(
+                        <tr key={key}>
+                            <td>{suggestionsData[key].label}</td>
+                            <td dangerouslySetInnerHTML={{ __html: suggestionsData[key]['Carbon Footprints']}}></td>
+                            <td dangerouslySetInnerHTML={{ __html: suggestionsData[key]['Alternative Idea']}}></td>
+                        </tr>))
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
